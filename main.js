24be3038fe7f42e21a6d4fa90a565b74538077dd
Steve Hansen <stevehansen@Steves-MacBook-Air.local>
2016-08-14 21:34:44 -0400

    cleanup of module

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
-(function (doc, nav) {
+( function( doc, nav ) {
 
-  var video, outputVideo, context, scratchCanvas, frame, backgroundData;
+  var video, outputVideo, scratchContext, scratchCanvas, frame, backgroundData;
 
   function initialize() {
 
   // The source video.
 
-    video = doc.getElementById("v");
-    outputVideo = doc.getElementById("greenv");
+    video = doc.getElementById( 'v' );
+    outputVideo = doc.getElementById( 'greenv' );
 
   // The replacement image
 
-    var backgroundCanvas = doc.getElementById("backcanvas");
-    var backgroundContext = backgroundCanvas.getContext("2d");
-    var img = document.getElementById("bb");
-    backgroundContext.drawImage(img, 300, 300, 1000, 1000, 0, 0, 1000,1000);
-
-    backgroundData = backgroundContext.getImageData( 0, 0, 820, 640).data;
-
- //var pixLength = backgroundData.length / 4;
- //console.log( 'pixLength:', pixLength );
- //console.log( backgroundData[0], backgroundData[1], backgroundData[2], backgroundData[3] );
- //console.log( backgroundData[2000], backgroundData[2001], backgroundData[2002], backgroundData[2003] );
+    var backgroundCanvas = doc.getElementById( 'backcanvas' );
+    var backgroundContext = backgroundCanvas.getContext( '2d' );
+    var img = document.getElementById( 'bb' );
+    backgroundContext.drawImage( img, 300, 300, 1000, 1000, 0, 0, 1000, 1000 );
+    backgroundData = backgroundContext.getImageData( 0, 0, 820, 640 ).data;
 
   // The working canvas.
 
-    scratchCanvas = doc.getElementById("c");
-    context = scratchCanvas.getContext("2d");
+    scratchCanvas = doc.getElementById( 'c' );
+    scratchContext = scratchCanvas.getContext( '2d' );
 
     nav.getUserMedia = ( navigator.getUserMedia ||
                        navigator.webkitGetUserMedia ||
                        navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+                       navigator.msGetUserMedia );
 
     // Get the webcam's stream.
 
-    nav.getUserMedia({video: true}, startStream, function() {});
+    nav.getUserMedia( { video: true }, startStream, function() {} );
   }
 
   function startStream( stream ) {
@@ -44,18 +38,19 @@
 
     createGreenStream();
 
-    requestAnimationFrame( draw );
+    requestAnimationFrame( drawCanvasFrame );
   }
 
-  function draw() {
-    frame = readFrame();
+  function drawCanvasFrame() {
 
+// render the green screen modified frames
+
+    frame = readFrame();
     if ( frame ) {
       replaceGreen( frame );
-      context.putImageData( frame, 0, 0 );
+      scratchContext.putImageData( frame, 0, 0 );
     }
-
-    requestAnimationFrame( draw );
+    requestAnimationFrame( drawCanvasFrame );
   }
 
   function createGreenStream() {
@@ -69,12 +64,16 @@
 
   function readFrame() {
     try {
-      context.drawImage( video, 0, 0, video.width, video.height );
-    } catch (e) {
+      scratchContext.drawImage( video, 0, 0, video.width, video.height );
+    } catch ( e ) {
+
       // The video may not be ready, yet.
       return null;
     }
-    return context.getImageData( 0, 0, video.width, video.height );
+
+// get the image data for the input video frame
+
+    return scratchContext.getImageData( 0, 0, video.width, video.height );
   }
 
   function replaceGreen( frame ) {
@@ -85,60 +84,60 @@
 
     // Convert from RGB to HSL...
 
-      var hsl = rgb2hsl(frame.data[j], frame.data[j + 1], frame.data[j + 2]);
+      var hsl = rgb2hsl( frame.data[j], frame.data[j + 1], frame.data[j + 2] );
       var h = hsl[0], s = hsl[1], l = hsl[2];
 
     // ... and check if we have a somewhat green background
 
-      if (h >= 90 && h <= 160 && s >= 25 && s <= 90 && l >= 20 && l <= 75) {
+      if ( h >= 90 && h <= 160 && s >= 25 && s <= 90 && l >= 20 && l <= 75 ) {
 
       //.. and replace with the background pixels
 
       frame.data[j] = backgroundData[ j ];
       frame.data[j + 1] = backgroundData[ j + 1 ];
-      frame.data[j + 2] = backgroundData[ j + 2 ]
-      frame.data[j + 3] = 255
+      frame.data[j + 2] = backgroundData[ j + 2 ];
+      frame.data[j + 3] = 255;
       }
     }
   }
 
-  function rgb2hsl(r, g, b) {
+  function rgb2hsl( r, g, b ) {
     r /= 255; g /= 255; b /= 255;
 
-    var min = Math.min(r, g, b);
-    var max = Math.max(r, g, b);
+    var min = Math.min( r, g, b );
+    var max = Math.max( r, g, b );
     var delta = max - min;
     var h, s, l;
 
-    if (max == min) {
+    if ( max == min ) {
       h = 0;
-    } else if (r == max) {
-      h = (g - b) / delta;
-    } else if (g == max) {
-      h = 2 + (b - r) / delta;
-    } else if (b == max) {
-      h = 4 + (r - g) / delta;
+    } else if ( r == max ) {
+      h = ( g - b ) / delta;
+    } else if ( g == max ) {
+      h = 2 + ( b - r ) / delta;
+    } else if ( b == max ) {
+      h = 4 + ( r - g ) / delta;
     }
 
-    h = Math.min(h * 60, 360);
+    h = Math.min( h * 60, 360 );
 
-    if (h < 0) {
+    if ( h < 0 ) {
       h += 360;
     }
 
-    l = (min + max) / 2;
+    l = ( min + max ) / 2;
 
-    if (max == min) {
+    if ( max == min ) {
       s = 0;
-    } else if (l <= 0.5) {
-      s = delta / (max + min);
+    } else if ( l <= 0.5 ) {
+      s = delta / ( max + min );
     } else {
-      s = delta / (2 - max - min);
+      s = delta / ( 2 - max - min );
     }
 
-    return [h, s * 100, l * 100];
+    return [ h, s * 100, l * 100 ];
   }
 
-  addEventListener("DOMContentLoaded", initialize);
+  addEventListener( 'DOMContentLoaded', initialize );
 
 } ) ( document, navigator );
